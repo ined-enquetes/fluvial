93106800dcafae719f304b1a2b76a1640dcfcf1c
Thibaud Ritzenthaler <thibaud.ritzenthaler@ined.fr>
2026-02-25 16:35:16 +0100

    :sparkles: Update home and template

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,11 @@ export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image src="/inedSquareTrans.png"
-          width="300" height="300" alt="Logo Ined"/>
-        <p>Welcome on Ined&apos;s survey tool</p>
+        <Image src="/fluvial_logo.png"
+          width="300" height="300" alt="Logo Fluvial"/>
+        <p>Welcome on Fluvial, <br/>DDI high part metadata solution by Ined</p>
       </main>
+      
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         
       </footer>
